c174faef99b02b17f81cd1f0ac7eeedb2a368cd6
hridoy281810 <hridoy01724281810@gmail.com>
2023-03-20 13:04:55 +0600

    api data load

--- js/countries.js
+++ js/countries.js
@@ -5,14 +5,20 @@ const loadCountries = () =>{
 }
 
 const displayCountries = countries =>{
-    console.log(countries);
+    console.log(countries[0]);
     const allCountriesHTML = countries.map(country => getCountryHTML(country))
-    console.log(allCountriesHTML)
+    // console.log(allCountriesHTML)
+    const container = document.getElementById('countries');
+    container.innerHTML = allCountriesHTML.join(' ')
+    
+
 } 
 const getCountryHTML = country => {
+
     return `
-    <div>
+    <div class="country">
     <h2>${country.name.common}</h2>
+    <img src="${country.flags.png}" alt="">
     </div>
     `;
 }
